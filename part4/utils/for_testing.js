85e896b9b457ce0df68566cbca562d8ed4c796e1
Duyen Nguyen <hongduyen0705@gmail.com>
2022-08-20 15:15:20 +0300

    part4 has 2 last exercises remaining

--- part4/utils/for_testing.js
+++ part4/utils/for_testing.js
@@ -1,24 +1,19 @@
-// functions 
+// functions
 
-const blog = require("../models/blog")
+const blog = require("../models/blog");
 
 const reverse = (string) => {
-    return string
-    .split('')
-    .reverse()
-    .join('')
-}
+  return string.split("").reverse().join("");
+};
 
 const average = (array) => {
-    const reducer = (sum, item) => {
-        return sum + item
-    }
-    return array.length === 0 ? 0 :  array.reduce(reducer, 0) / array.length
-}
-
-
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+  return array.length === 0 ? 0 : array.reduce(reducer, 0) / array.length;
+};
 
 module.exports = {
-    reverse,
-    average,
-}
+  reverse,
+  average,
+};
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,30 +1,36 @@
-
-// define a dummy function that receives an array of blog posts as a parameter and always returns the value 1. 
+// define a dummy function that receives an array of blog posts as a parameter and always returns the value 1.
 const dummy = (blogs) => {
-    return 1;
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-    return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => {
-        if(!blog.likes) return 0;
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => {
+        if (!blog.likes) return 0;
         return sum + blog.likes;
-    }, 0)
-}
+      }, 0);
+};
 
+// 4.5
 const favoriteBlog = (blogs) => {
- const mostLikedBlog = blogs.reduce(function(prev, current) {
-    return prev.likes > current.likes ? prev : current
-   }, 0)
- 
-  const {title, author, likes} = mostLikedBlog
-  return {title, author, likes};
-}
+  const mostLikedBlog = blogs.reduce(function (prev, current) {
+    return prev.likes > current.likes ? prev : current;
+  }, 0);
+  const { title, author, likes } = mostLikedBlog;
+  return { title, author, likes };
+};
 
+const mostBlogs = (blogs) => {
+  // authors array
+  const authors = blogs.map((blog) => blog.author);
+};
+
+const mostLikes = (blogs) => {};
 // 4.6 mostBlogs
 
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog
-}
-
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
