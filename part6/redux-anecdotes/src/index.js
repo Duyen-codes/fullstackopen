fa6ca1d497d620668c250bea34864f1eb1f951d3
Duyen Nguyen <hongduyen0705@gmail.com>
2022-08-29 17:58:04 +0300

    install redux toolkit

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import anecdoteReducer from "./reducers/anecdoteReducer";
 
-const store = createStore(reducer)
+const store = createStore(anecdoteReducer);
+console.log("state of store: ", store.getState());
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
