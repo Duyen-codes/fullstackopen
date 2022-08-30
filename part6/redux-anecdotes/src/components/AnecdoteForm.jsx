e66011ab9a1eb41d4485818f171c81c13599884e
Duyen Nguyen <hongduyen0705@gmail.com>
2022-08-30 08:38:14 +0300

    done 6.13-6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,14 +1,16 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(createAnecdote(newAnecdote));
   };
   return (
     <div>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,59 +1,37 @@
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
+import { createSlice } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-// a reducer is a function which is given the current state and an action as params, it returns a new state
-const reducer = (state = initialState, action) => {
-  if (action.type === "VOTE") {
-    const id = action.data.id;
-    const noteToChange = state.find((n) => n.id === id);
-    const changedNote = {
-      ...noteToChange,
-      votes: noteToChange.votes + 1,
-    };
-    return state.map((note) => (note.id !== id ? note : changedNote));
-  }
-
-  if (action.type === "NEW_ANECDOTE") {
-    return state.concat(action.data);
-  }
-
-  return state;
-};
-
-export default reducer;
-
-export const createAnecdote = (content) => {
-  return {
-    type: "NEW_ANECDOTE",
-    data: {
-      content,
-      id: getId(),
-      votes: 0,
+const anecdoteSlice = createSlice({
+  name: "anecdotes",
+  initialState: [],
+  reducers: {
+    createAnecdote(state, action) {
+      state.push(action.payload);
     },
-  };
-};
 
-export const addVote = (id) => {
-  return {
-    type: "VOTE",
-    data: { id },
-  };
-};
+    addVote(state, action) {
+      const id = action.payload;
+      const anecdoteToChange = state.find((n) => n.id === id);
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      };
+
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      );
+    },
+
+    appendAnecdote(state, action) {
+      state.push(action.payload);
+    },
+
+    // setAnecdotes action/ action creator setAnecdotes
+    setAnecdotes(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { createAnecdote, addVote, appendAnecdote, setAnecdotes } =
+  anecdoteSlice.actions;
+export default anecdoteSlice.reducer;
