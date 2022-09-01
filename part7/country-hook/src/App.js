a8c92ce56e0d5b085411b6765d27615dd87c2836
Duyen Nguyen <hongduyen0705@gmail.com>
2022-09-01 13:11:45 +0300

    done 7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -3,14 +3,11 @@ import axios from "axios";
 
 // useField custom hook
 const useField = (type) => {
-  console.log("TYPE inside useField: ", type);
   const [value, setValue] = useState("");
-
   const onChange = (event) => {
     setValue(event.target.value);
   };
 
-  console.log("VALUE inside useField: ", value);
   return {
     type,
     value,
@@ -20,44 +17,28 @@ const useField = (type) => {
 
 // useCountry custom hook
 const useCountry = (name) => {
-  console.log("NAME: ", name);
   const [country, setCountry] = useState(null);
 
   useEffect(() => {
+    setCountry(null); //without this, the app does not work
     if (name) {
       const fetchCountry = async () => {
-        {
-          try {
-            const response = await axios.get(
-              `https://restcountries.com/v3.1/name/${name}?fullText=true`
-            );
-
-            console.log("RESPONSE: ", response.data);
-            setCountry(response.data);
-          } catch (error) {
-            console.log(error);
-            setCountry(null);
-          }
-        }
+        const response = await axios.get(
+          `https://restcountries.com/v3.1/name/${name}?fullText=true`
+        );
+        setCountry(response.data[0]);
       };
       fetchCountry();
     }
   }, [name]);
-  console.log("COUNTRY inside useCountry: ", country);
-
   return country;
 }; // end of useCountry custom hook
 
 const Country = ({ country }) => {
-  console.log("COUNTRY inside country component: ", country);
-  console.log("TYPEOF COUNTRY: ", typeof country);
   if (!country) {
-    return null;
+    return <div>not found...</div>;
   }
 
-  if (!country.found) {
-    <div>not found...</div>;
-  }
   return (
     <div>
       <h3>{country?.name?.common}</h3>
@@ -76,14 +57,10 @@ const App = () => {
   const nameInput = useField("text");
   const [name, setName] = useState("");
   const country = useCountry(name);
-  console.log("COUNTRY in APP: ", country);
-  console.log("COUNTRY custom hook from APP.js: ", country);
+
   const fetch = (e) => {
     e.preventDefault();
     setName(nameInput.value);
-    console.log("find clicked");
-    console.log("nameInput.value: ", nameInput.value);
-    console.log("NAME STATE inside APP: ", name);
   };
 
   return (
