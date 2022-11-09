1b01a4498efa30b591e03b2ecf1a3ac4bb793390
Duyen-codes <hongduyen0705@gmail.com>
2022-11-09 18:46:31 +0200

    added webpack-dev-server

--- part7/webpack-part7/src/App.js
+++ part7/webpack-part7/src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.css";
+
 const App = () => {
-  return <div className="container">hello webpack</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <p>is this automatic showing. It seems like that.</p>
+      <button onClick={() => setCounter(counter + 1)}>press</button>
+    </div>
+  );
 };
 
 export default App;
