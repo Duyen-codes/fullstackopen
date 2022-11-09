492a0449eb45af057f6bec70d74b6d474315dce5
Duyen-codes <hongduyen0705@gmail.com>
2022-11-09 16:43:04 +0200

    webpack

---
+++ part7/webpack-part7/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
+
+module.exports = config;
