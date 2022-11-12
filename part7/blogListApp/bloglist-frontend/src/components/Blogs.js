6fe229e7058605bfd63be3b7a73beead8c50b95f
Duyen-codes <hongduyen0705@gmail.com>
2022-11-12 12:11:50 +0200

    done part7

--- part7/blogListApp/bloglist-frontend/src/components/Blogs.js
+++ part7/blogListApp/bloglist-frontend/src/components/Blogs.js
@@ -1,26 +1,52 @@
 import React from "react";
-import Blog from "./Blog";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 
-const Blogs = ({ blogs, likeBlog, removeBlog }) => {
-  if (!blogs) {
-    return null;
-  }
+const Blogs = () => {
+  const blogs = useSelector((state) => state.blogs);
   return (
     <div>
       <h2>Blogs</h2>
-      <div className="blogs">
-        {blogs.map((blog) => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-            blogs={blogs}
-            likeBlog={likeBlog}
-            removeBlog={removeBlog}
-          />
-        ))}
-      </div>
+      <TableContainer component={Paper} className="blogs" id="blogs">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>blogs</TableCell>
+              <TableCell>users</TableCell>
+            </TableRow>
+            {blogs.map((blog) => (
+              <TableRow key={blog.id}>
+                <TableCell>
+                  <Link to={`/blogs/${blog.id}`}>
+                    {blog.title} by {blog.author}
+                  </Link>
+                </TableCell>
+                <TableCell>{blog.user.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
 
 export default Blogs;
+
+{
+  /* <Blog
+            key={blog.id}
+            blog={blog}
+            blogs={blogs}
+            likeBlog={likeBlog}
+            removeBlog={removeBlog}
+          /> */
+}
