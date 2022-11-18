a45127be68942c921420a02b5daca1dbd0933581
Duyen-codes <hongduyen0705@gmail.com>
2022-11-18 14:51:56 +0200

    ex 8.8 implemented an Authors view to show details of all authors on a page

--- part8-graphQL/library/library-frontend/src/components/Authors.js
+++ part8-graphQL/library/library-frontend/src/components/Authors.js
@@ -1,30 +1,31 @@
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+	if (!props.show) {
+		return null;
+	}
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+	const authors = props.authors;
 
-export default Authors
+	return (
+		<div>
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>born</th>
+						<th>books</th>
+					</tr>
+					{authors.map((a) => (
+						<tr key={a.name}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Authors;
--- part8-graphQL/library/library-frontend/src/index.js
+++ part8-graphQL/library/library-frontend/src/index.js
@@ -1,5 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+	ApolloClient,
+	ApolloProvider,
+	gql,
+	InMemoryCache,
+} from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const client = new ApolloClient({
+	uri: "http://localhost:4000",
+	cache: new InMemoryCache(),
+});
+
+const query = gql`
+	query {
+		allAuthors {
+			name
+			born
+			bookCount
+		}
+	}
+`;
+
+client.query({ query }).then((result) => console.log(result));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+);
