2ae7080662467389d5bb14ef71ef7db44f4da77a
Duyen-codes <hongduyen0705@gmail.com>
2022-11-18 10:36:50 +0200

    done updating the cache

--- part8-graphQL/apollo-client/src/App.js
+++ part8-graphQL/apollo-client/src/App.js
@@ -1,25 +1,24 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+
+import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
+import { ALL_PERSONS } from "./queries";
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS);
-  console.log("result", result);
+	const result = useQuery(ALL_PERSONS);
+	console.log("result", result);
 
-  if (result.loading) {
-    return <div>loading...</div>;
-  }
+	if (result.loading) {
+		return <div>loading...</div>;
+	}
 
-  return <Persons persons={result.data.allPersons} />;
+	return (
+		<div>
+			<Persons persons={result.data.allPersons} />
+			<PersonForm />
+		</div>
+	);
 };
 
 export default App;
--- part8-graphQL/apollo-client/src/components/Persons.js
+++ part8-graphQL/apollo-client/src/components/Persons.js
@@ -1,19 +1,7 @@
 import { useState } from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 
-const FIND_PERSON = gql`
-  query findPersonByName($nameToSearch: String!) {
-    findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
-    }
-  }
-`
+import { FIND_PERSON } from '../queries'
 
 const Person = ({ person, onClose }) => {
   return (
@@ -34,6 +22,7 @@ const Persons = ({ persons }) => {
     variables: { nameToSearch },
     skip: !nameToSearch,
   })
+  console.log('result', result)
 
   if (nameToSearch && result.data) {
     return (
