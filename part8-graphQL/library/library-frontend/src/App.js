4f701bd92b6ef64bee4c0c1fe870611729155b0e
Duyen-codes <hongduyen0705@gmail.com>
2022-11-18 14:58:50 +0200

    done ex 8.9 implemented books view to show details of all books on a page

--- part8-graphQL/library/library-frontend/src/App.js
+++ part8-graphQL/library/library-frontend/src/App.js
@@ -3,13 +3,14 @@ import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import { useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "./queries";
+import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
 
 const App = () => {
 	const [page, setPage] = useState("authors");
 	const result = useQuery(ALL_AUTHORS);
+	const bookResult = useQuery(ALL_BOOKS);
 
-	if (result.loading) {
+	if (result.loading && bookResult.loading) {
 		return <div>loading...</div>;
 	}
 	return (
@@ -22,7 +23,7 @@ const App = () => {
 
 			<Authors show={page === "authors"} authors={result.data.allAuthors} />
 
-			<Books show={page === "books"} />
+			<Books show={page === "books"} books={bookResult.data.allBooks} />
 
 			<NewBook show={page === "add"} />
 		</div>
