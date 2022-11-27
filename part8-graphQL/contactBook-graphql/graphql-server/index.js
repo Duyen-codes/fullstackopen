7c937ef1c6fab41ccd7191a9fde42669f1f17ed1
Duyen-codes <hongduyen0705@gmail.com>
2022-11-27 18:38:11 +0200

    removed schema definition from index.js

--- part8-graphQL/contactBook-graphql/graphql-server/index.js
+++ part8-graphQL/contactBook-graphql/graphql-server/index.js
@@ -4,6 +4,8 @@ const {
 	gql,
 	AuthenticationError,
 } = require("apollo-server");
+
+const typeDefs = require("./schema");
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "TOP_SECRET";
@@ -49,59 +51,6 @@ let persons = [
 		id: "3d599471-3436-11e9-bc57-8b80ba54c431",
 	},
 ];
-
-const typeDefs = gql`
-	type User {
-		username: String!
-		friends: [Person!]!
-		id: ID!
-	}
-
-	type Token {
-		value: String!
-	}
-
-	type Address {
-		street: String!
-		city: String!
-	}
-
-	type Person {
-		name: String!
-		phone: String
-		address: Address!
-		id: ID!
-	}
-
-	enum YesNo {
-		YES
-		NO
-	}
-
-	type Query {
-		personCount: Int!
-		allPersons(phone: YesNo): [Person!]!
-		findPerson(name: String!): Person
-		me: User
-	}
-
-	type Mutation {
-		addPerson(
-			name: String!
-			phone: String
-			street: String!
-			city: String!
-		): Person
-
-		editNumber(name: String!, phone: String!): Person
-
-		createUser(username: String!): User
-
-		login(username: String!, password: String!): Token
-
-		addAsFriend(name: String!): User
-	}
-`;
 
 const resolvers = {
 	Query: {
