b058d46247317874a0cd119fb245f2db295d8702
Duyen-codes <hongduyen0705@gmail.com>
2022-11-29 15:05:38 +0200

    added friendOf field to each Person object, modified file model/person.js

--- part8-graphQL/contactBook-graphql/graphql-server/models/person.js
+++ part8-graphQL/contactBook-graphql/graphql-server/models/person.js
@@ -20,6 +20,12 @@ const personSchema = new mongoose.Schema({
 		required: true,
 		minLength: 3,
 	},
+	friendOf: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	],
 });
 
 module.exports = mongoose.model("Person", personSchema);
