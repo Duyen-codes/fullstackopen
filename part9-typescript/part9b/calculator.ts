a5bb19dcdfac580f8c9f7e062f4b12dc64441bd3
Duyen-codes <hongduyen0705@gmail.com>
2022-12-05 13:01:43 +0200

    first step with typescript

--- part9-typescript/part9b/calculator.ts
+++ part9-typescript/part9b/calculator.ts
@@ -1,17 +1,33 @@
 // create a type using typescript keyword type
 
 type Operation = 'multiply' | 'add' | 'divide'
-type Result = string | number
+type Result = number
 
 const calculator = (a: number, b: number, op: Operation): Result => {
-  if ((op = 'multiply')) {
-    return a * b
-  } else if ((op = 'add')) {
-    return a + b
-  } else if ((op = 'divide')) {
-    if (b === 0) return 'this cannot be done'
-    return a / b
+  switch (op) {
+    case 'multiply':
+      return a * b
+    case 'divide':
+      if (b === 0) throw new Error("Can't divide by 0!")
+      return a / b
+
+    case 'add':
+      return a + b
+    default:
+      throw new Error('Operation is not multiply, add or divide!')
   }
 }
 
-calculator(1, 2, 'yolo')
+try {
+  console.log(calculator(1, 5, 'divide'))
+} catch (error) {
+  let errorMessage = 'Something went wrong'
+
+  // object instanceof constructor
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log('errorMessage', errorMessage)
+}
+
+console.log(process.argv)
