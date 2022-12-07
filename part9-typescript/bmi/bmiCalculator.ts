6a91c5a83519ea894a65993b7a1a5a49535997c3
Duyen-codes <hongduyen0705@gmail.com>
2022-12-07 12:04:04 +0200

    done 9.1-9.1

--- part9-typescript/bmi/bmiCalculator.ts
+++ part9-typescript/bmi/bmiCalculator.ts
@@ -1,11 +1,11 @@
 const calculateBmi = (heightInCm: number, weightInKg: number) => {
   const result = weightInKg / Math.pow(heightInCm * 0.01, 2)
   if (result < 18.5) {
-    console.log(`underweight`)
+    console.log(`Underweight`)
   } else if (18.5 < result && result < 25) {
     console.log('Normal (healthy weight)')
   } else if (result >= 25 && result < 30) {
-    console.log('overweight')
+    console.log('Overweight')
   } else console.log('obesity')
 }
 
--- part9-typescript/bmi/exerciseCalculator.ts
+++ part9-typescript/bmi/exerciseCalculator.ts
@@ -1,6 +1,6 @@
 // func calculateExercises
 // returns an object that includes the following values
-interface Result {
+interface resultObject {
   periodLength: number
   trainingDays: number
   success: boolean
@@ -9,3 +9,48 @@ interface Result {
   target: number
   average: number
 }
+
+const calculateExercises = (
+  hourArr: Array<number>,
+  targetAmount: number,
+): resultObject => {
+  const periodLength = hourArr.length
+  const trainingDaysArr = hourArr.filter((d) => d !== 0)
+  const trainingDays = trainingDaysArr.length
+  let rating, ratingDescription, success
+  if (trainingDays < 3) {
+    rating = 1
+    ratingDescription = 'not bad, keep pushing'
+  } else if (trainingDays >= 5) {
+    rating = 2
+    ratingDescription = 'not too bad but could be better'
+  } else {
+    rating = 3
+    ratingDescription = 'great job'
+  }
+  console.log('trainingDays', trainingDays)
+  console.log('periodLength', periodLength)
+  const hourSum = hourArr.reduce((sum, value) => {
+    return sum + value
+  }, 0)
+  const average = hourSum / hourArr.length
+
+  console.log('average', average)
+  if (average === targetAmount) {
+    success = true
+  } else success = false
+  console.log('rating', rating)
+  console.log('ratingDescription', ratingDescription)
+  console.log('targetAmount', targetAmount)
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target: targetAmount,
+    average,
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
