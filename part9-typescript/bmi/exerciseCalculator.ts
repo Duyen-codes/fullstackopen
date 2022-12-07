4e8496a80ac241258408f04ca1a4eb3d4dd0f00a
Duyen-codes <hongduyen0705@gmail.com>
2022-12-07 11:02:00 +0200

    added Result interface, created npm script to call the func with hard-coded values

---
+++ part9-typescript/bmi/exerciseCalculator.ts
@@ -0,0 +1,11 @@
+// func calculateExercises
+// returns an object that includes the following values
+interface Result {
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
+}
