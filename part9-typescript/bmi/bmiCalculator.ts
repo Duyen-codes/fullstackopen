c6a69ed8d07a50b1a820719ca0dc5ac3db2aebd6
Duyen-codes <hongduyen0705@gmail.com>
2022-12-27 14:06:26 +0200

    make bmiCalculator work with command line arguments

--- part9-typescript/bmi/bmiCalculator.ts
+++ part9-typescript/bmi/bmiCalculator.ts
@@ -1,3 +1,21 @@
+interface bmiValues {
+  heightInCm: number
+  weightInKg: number
+}
+
+const parseArguments = (args: Array<string>): bmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      heightInCm: Number(args[2]),
+      weightInKg: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers')
+  }
+}
+
 const calculateBmi = (heightInCm: number, weightInKg: number) => {
   const result = weightInKg / Math.pow(heightInCm * 0.01, 2)
   if (result < 18.5) {
@@ -9,4 +27,15 @@ const calculateBmi = (heightInCm: number, weightInKg: number) => {
   } else console.log('obesity')
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { heightInCm, weightInKg } = parseArguments(process.argv)
+  calculateBmi(heightInCm, weightInKg)
+} catch (error) {
+  let errorMessage = 'something bad happened'
+  if (error instanceof Error) {
+    errorMessage += ' Error ' + error.message
+  }
+  console.log(errorMessage)
+}
+
+// console.log(calculateBmi(180, 74))
