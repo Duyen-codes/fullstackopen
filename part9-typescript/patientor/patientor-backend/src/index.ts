270e23bf94a7c7d3e9ce713ee0aa6e5ca6902b22
Duyen-codes <hongduyen0705@gmail.com>
2023-01-02 13:54:50 +0200

    done 9.10-9.11

--- part9-typescript/patientor/patientor-backend/src/index.ts
+++ part9-typescript/patientor/patientor-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 const app = express()
 import diagnoseRouter from './routes/diagnoses'
+import patientRouter from './routes/patients'
 app.use(express.json())
 
 const PORT = 3000
@@ -11,6 +12,8 @@ app.get('/ping', (_req, res) => {
 })
 
 app.use('/api/diagnoses', diagnoseRouter)
+app.use('/api/patients', patientRouter)
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
