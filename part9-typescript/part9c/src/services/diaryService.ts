0069be15b9a0815213d62b887ec2887ced6668f9
Duyen-codes <hongduyen0705@gmail.com>
2023-01-02 10:29:24 +0200

    done Utility Types for the route which returns all diary entries

--- part9-typescript/part9c/src/services/diaryService.ts
+++ part9-typescript/part9c/src/services/diaryService.ts
@@ -1,12 +1,21 @@
 import diaries from '../../data/diaries'
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types'
 
-const getEntries = (): Array<DiaryEntry> => {
+const getEntries = (): DiaryEntry[] => {
   return diaries
 }
 
-const addDiary = () => {
-  return null
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }))
 }
 
-export default { getEntries, addDiary }
+const addDiary = () => {
+  return []
+}
+
+export default { getEntries, addDiary, getNonSensitiveEntries }
