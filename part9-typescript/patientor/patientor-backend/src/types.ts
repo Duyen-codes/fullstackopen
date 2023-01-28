1a8438156ef1fa2bbc782121041085ce0fea4480
Duyen-codes <hongduyen0705@gmail.com>
2023-01-28 10:33:55 +0200

    expand backend types

--- part9-typescript/patientor/patientor-backend/src/types.ts
+++ part9-typescript/patientor/patientor-backend/src/types.ts
@@ -22,3 +22,17 @@ export enum Gender {
 export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>
 
 export type NewPatientEntry = Omit<PatientEntry, 'id'>
+
+export interface Entry {}
+
+export interface Patient {
+  id: string
+  name: string
+  ssn: string
+  occupation: string
+  gender: Gender
+  dateOfBirth: string
+  entries: Entry[]
+}
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>
